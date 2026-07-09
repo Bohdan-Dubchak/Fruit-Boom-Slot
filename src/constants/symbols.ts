2090267efeef1ea0e6446fc59c6c87a64b88dc47
Bohdan Dubchak <dubchakbohdan63@gmail.com>
2026-07-09 21:01:11 +0200

    first: create all symbol and number % random

---
+++ src/constants/symbols.ts
@@ -0,0 +1,14 @@
+export const SYMBOLS_IDS: Record<string, number> = {
+    'symbol_0': 1,
+    'symbol_1': 5,
+    'symbol_2': 15,
+    'symbol_3': 20,
+    'symbol_4': 25,
+    'symbol_5': 30,
+    'symbol_6': 30,
+    "symbol_7": 10,
+    "symbol_18": 10,
+    "symbol_21": 10
+};
+
+export const SYMBOLS_LIST: string[] = Object.keys(SYMBOLS_IDS);
