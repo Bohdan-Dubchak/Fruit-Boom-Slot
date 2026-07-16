76da50454732eca053ed8f003d9f0882f4ea8ef4
Bohdan Dubchak <dubchakbohdan63@gmail.com>
2026-07-16 20:39:41 +0200

    first: render btn minus and plus

--- src/ui/UIFactory.ts
+++ src/ui/UIFactory.ts
@@ -1,13 +1,18 @@
 import {Container} from "pixi.js";
 import {SpinButton} from "./spinBtn/spinBtn.ts";
+import {PlusBet} from "./button/plusButton.ts";
+import {MinusBet} from "./button/minusButton.ts";
 
 export class UIFactory {
 
     createGameUI(onSpin: () => void): { elements: Container[], spinButton: SpinButton } {
         const spinButton = new SpinButton(onSpin);
 
+        const plusButton = new PlusBet();
+        const minusButton = new MinusBet();
+
         return {
-            elements: [spinButton],
+            elements: [spinButton, plusButton, minusButton],
             spinButton
         }
     };
